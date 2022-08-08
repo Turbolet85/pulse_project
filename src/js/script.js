fbca4cd922694e1cd5c288412a005a11c3b9194d
Turbolet85 <turbolet85@gmail.com>
2022-08-08 07:08:21 +0200

    last commit

--- src/js/script.js
+++ src/js/script.js
@@ -106,9 +106,26 @@ $(document).ready(function(){
 			data: $(this).serialize()
 		}).done(function() {
 			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn();
 
 			$('form').trigger('reset');
 		});
+		return false;
+	});
+
+	//Scroll up
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+	$("a[href=#up]").click(function() {
+		const _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
 		return false;
 	});
 
